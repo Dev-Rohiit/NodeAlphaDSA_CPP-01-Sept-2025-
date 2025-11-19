3430f7dc108447116f61f407e8bf2e443b5cd28c
Dev-Rohiit <kushwaharohit908@gmail.com>
2025-11-19 10:37:16 +0530

    char array

--- Lecture 25/004CharArrays_ReadInput.cpp
+++ Lecture 25/004CharArrays_ReadInput.cpp
@@ -3,30 +3,28 @@
 #include<iostream>
 using namespace std;
 
-void ReadStr(char str[], int delimeter = '\n'){
-	char ch;
-	int i = 0;
-	while (true)
-	{
-		if (ch == delimeter)
-		{
-			break;
-		}
-		ch = cin.get();
-		str[i] = ch;
-		i++;
-		
-	}
-	str[i] = '\0';
-	
-}
+// void ReadStr(char str[], int delimiter = '\n'){
+// 	char ch;
+// 	int i = 0;
+// 	while (true)
+// 	{
+// 		if (ch == delimiter)
+// 		{
+// 			break;
+// 		}
+// 		ch = cin.get();
+// 		str[i] = ch;
+// 		i++;
+// 	}
+// 	str[i] = '\0';
+// }
 int main() {
 	char str[101];
 
 	cout << "Enter a string : ";
 	// cin>>str;
-	ReadStr(str, '$');
-
+	// ReadStr(str, '$');
+	cin.getline(str,101);
 	cout << "You've entered : " << str << endl;
 
 	return 0;
--- Lecture 25/005CharArrays_Compare.cpp
+++ Lecture 25/005CharArrays_Compare.cpp
@@ -5,47 +5,37 @@
 
 using namespace std;
 
-int stringCompare(char* str1, char* str2) {
+// int stringCompare(char* str1, char* str2){
+// 	int i = 0;
+// 	int j = 0;
+// 	while (str1[i]!='\0' && str2[j]!='\0')
+// 	{
+// 		if (str1[i]>str2[j])
+// 		{
+// 			return 1;
+// 		}
+// 		else if(str1[i]<str2[j]){
+// 			return -1;
+// 		}
+// 		else{
+// 			i++;
+// 			j++;
+// 		}
+		
+// 	}
+// 	if (str1[i]=='\0' && str2[j]=='\0')
+// 	{
+// 		return 0;
+// 	}else if(str1[i]=='\0'){
+// 		return -1;
+// 	}else{
+// 		return 1;
+// 	}
+// }
+int main(){
 
-	int i = 0; // to iterate over the char. of str1
-	int j = 0; // to iterate over the char. of str2
-
-	while(str1[i] != '\0' and str2[j] != '\0') {
-
-		if(str1[i] > str2[j]) {
-			// str1 is greater than str2
-			return 1;
-		} else if(str1[i] < str2[j]) {
-			// str2 is greater than str1
-			return -1;
-		} else {
-			i++;
-			j++;
-		}
-
-	} 
-
-	if(str1[i] == '\0' and str2[j] == '\0') {
-		// length of str1 is equal to length of str2,
-		// therefore they must be equal
-		return 0;
-	} else if(str1[i] == '\0') {
-		// length of str2 is greater than length of str1
-		// therefore str2 > str1
-		return -1;
-	} else {
-		// length of str1 is greater than length of str2
-		// therefore str1 > str2
-		return 1;
-	}
-
-
-}
-
-int main() {
-
-	// char str1[] = "abc";
-	// char str2[] = "abc";
+	// char str1[] = "aaaaa";
+	// char str2[] = "aaaa";
 
 	char str1[101];
 	char str2[101];
@@ -54,10 +44,9 @@ int main() {
 	cin.getline(str2, 101);
 
 	// int out = stringCompare(str1, str2);
-
 	int out = strcmp(str1, str2);
 
-	if(out == 0) {else
+	if(out == 0){
 		cout << str1 << " is equal to " << str2 << endl;
 	} else if(out > 0) {
 		cout << str1 << " is greater than " << str2 << endl;
